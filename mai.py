735ba23406ff0284183bdbef5cbdcc761fa2c200
EsraaAlhabbash <135063242+EsraaAlhabbash@users.noreply.github.com>
2023-06-21 16:31:12 +0300

    Update mai.py

--- mai.py
+++ mai.py
@@ -9,6 +9,16 @@ Delivery Date : 21-6-2023
 # course_id (generated using uuid4) ,
 # course name (user_input) and
 # course mark (user_input)
+solution
+import uuid
+
+
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()
+        self.course_name = course_name
+        self.course_mark = course_mark
+
 
 
 class Student:
